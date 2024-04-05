00ba2e0531832b454aca24a5265adc088cea9ffb
InhyeKang <kanginhae0826@gmail.com>
2024-04-05 09:56:33 +0900

    setting :: Sprint Boot 프로젝트 생성

---
+++ myfintech/src/main/java/com/inhyekang/myfintech/MyfintechApplication.java
@@ -0,0 +1,13 @@
+package com.inhyekang.myfintech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyfintechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyfintechApplication.class, args);
+	}
+
+}
---
+++ myfintech/src/test/java/com/inhyekang/myfintech/MyfintechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inhyekang.myfintech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyfintechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
